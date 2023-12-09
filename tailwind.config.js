0df5c8dbd71953ec5ea0b2e72a7af94a6b6a8b79
Franck <49774188+fishem@users.noreply.github.com>
2023-12-09 11:04:01 +0800

    update the headers

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "food-image":
+          "url('src/img/lampos-aritonang-24gR_9lCdes-unsplash 1.png')",
+      },
+      colors: {
+        yellow: "#FFD15B",
+      },
+      fontFamily: {
+        manrope: ['"Manrope"', ...defaultTheme.fontFamily.sans],
+        anton: ['"Anton"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
-
+};
